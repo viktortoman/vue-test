16302609fb37dc3a2578dc45f988f930543e07b9
Viktor <viktor.toman19@gmail.com>
2023-04-17 09:58:48 +0200

    init: scss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,7 +40,9 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
+$color: red;
+
 h3 {
   margin: 40px 0 0;
 }
