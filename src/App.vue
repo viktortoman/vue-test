4579f7e4b852cc40f1d32612b43c15345b37fb30
Viktor <viktor.toman19@gmail.com>
2023-04-17 11:09:01 +0200

    config tailwind and make main component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container mx-auto rounded-xl shadow border p-8 m-10">
+        <MainComponent></MainComponent>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MainComponent from "@/components/Main.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        MainComponent
+    }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+html {
+    background: #262626;
+
+    body {
+        max-width: 1920px;
+        max-height: 950px;
+    }
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card">
+        <div>Rocket League Showdown 2020 Summer Shuffle</div>
+        <div>June 29 - July 12</div>
+        <div>
+            <div>EterNaLeNvyEterNaLeNvy</div>
+            <div>DakotaCox</div>
+            <div>Sarah Jessica Parker</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MainComponent"
+}
+</script>
+
+<style scoped>
+    .card {
+
+    }
+</style>
